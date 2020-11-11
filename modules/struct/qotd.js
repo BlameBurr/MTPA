ce86713013d013aa991a45fdd31b467db33a7ca5
James Walker <33370725+Bri-ish@users.noreply.github.com>
2020-11-11 00:05:57 +0000

    Create qotd.js

---
+++ modules/struct/qotd.js
@@ -0,0 +1,10 @@
+module.exports.quotes = [
+    "I think you ought to know I'm feeling very depressed.",
+    "Life? Don't talk to me about life.",
+    "Marvin: I've been communicating with the ship.\nFord: What did it say?\nMarvin: It hates me.",
+    "There's only one life-form as intelligent as me within thirty parsecs of here and that's me.",
+    "I'm fifty thousand times more intelligent than you and even I don't know the answer. It gives me a headache just trying to think down to your level.",
+    "You think you've got problems? What are you supposed to do if you are a manically depressed robot? No, don't try to answer that.",
+    `I got very bored and depressed, so I went and plugged myself in to its external computer feed. I talked to the computer at great length and explained my view of the Universe to it," said Marvin.\n"And what happened?" pressed Ford.\n"It committed suicide," said Marvin`,
+    "It's part of the shape of the Universe. I only have to talk to somebody and they begin to hate me."
+];
